93185122b36ed1f54be68e0933895086c76636b2
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2017-07-31 12:34:29 -0700

    pandeia_sat_sens now makes a map showing the well fraction.

--- pandeia_sat_sens.py
+++ pandeia_sat_sens.py
@@ -2,20 +2,34 @@
 # Import relevant libraries
 ## Following example from 
 from pandeia.engine.perform_calculation import perform_calculation
+from pandeia.engine.instrument_factory import InstrumentFactory
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import yaml
 import pdb
 
-calc = yaml.load(open('yaml/default_nc_lw_imaging.yaml'))
+params = yaml.load(open('yaml/grism_example.yaml'))
 
-result = perform_calculation(calc)
+## Get the full well depth from the instrument parameters
+instrument = InstrumentFactory(params['configuration'])
+det_pars = instrument.get_detector_pars()
+fullWell = det_pars['fullwell']
+
+result = perform_calculation(params)
+
+## Gets the well fraction
+wellFrac = result['information']['exposure_specification']['tsat'] * result['2d']['detector'] / det_pars['fullwell']
 
 fig, ax = plt.subplots()
 
-imMap = ax.imshow(result['2d']['saturation'])
-ax.set_title('Saturation Map')
+imMap = ax.imshow(wellFrac)
+ax.set_title('Well Fraction Map Map')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
-fig.colorbar(imMap)
+
+## Adjust colorbar height to match image
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.05)
+fig.colorbar(imMap,label='Well Fraction',cax=cax)
 fig.show()
 pdb.set_trace()
